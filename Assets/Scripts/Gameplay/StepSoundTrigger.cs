937b9676758b6df1e587e273ca7809871cffd536
Plyukh <plyukh.2015@gmail.com>
2025-11-12 21:50:46 +0300

    Add StepSoundTriggers

---
+++ Assets/Scripts/Gameplay/StepSoundTrigger.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class StepSoundTrigger : MonoBehaviour
+{
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip clip;
+
+    private bool soundPlayed;
+
+    public void PlaySound()
+    {
+        audioSource.PlayOneShot(clip);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (soundPlayed == false)
+        {
+            if (other.gameObject.GetComponent<PlayerMovementController>())
+            {
+                PlaySound();
+            }
+        }
+    }
+}
